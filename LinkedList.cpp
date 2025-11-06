013839d0d52f164d030756aa19648beb94d1e014
Michael Priore <162520748+mrp5623@users.noreply.github.com>
2025-11-06 22:37:37 +0000

    Linked list prints

---
+++ LinkedList.cpp
@@ -0,0 +1,20 @@
+#include "LinkedList.hpp"
+#include <iostream>
+
+
+
+void LinkedList::printForward() const {
+    Node* curr = head;
+    while (curr.next){
+        std::cout << curr.data << std::endl;
+        curr = curr.next;
+    }
+}
+
+void LinkedList::printForward() const {
+    Node* curr = tail;
+    while (curr.prev){
+        std::cout << curr.data << std::endl;
+        curr = curr.prev;
+    }
+}
