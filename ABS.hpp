4dc23810714bc63a4137d01ab56505a31f7df4a7
Michael Priore <162520748+mrp5623@users.noreply.github.com>
2025-11-07 19:05:33 +0000

    peek n pop

--- ABS.hpp
+++ ABS.hpp
@@ -11,8 +11,16 @@ template<typename T>
 class ABS : public StackInterface<T> {
 public:
     // Big 5 + Parameterized Constructor
-    ABS();
-    explicit ABS(const size_t capacity);
+    ABS(){
+        capacity_ = 1;
+        curr_size_ = 0;
+        array_ = new T[capacity_];
+    }
+    explicit ABS(const size_t capacity){
+        capacity_ = capacity;
+        curr_size_ = 0;
+        array_ = new T[capacity_];
+    }
     ABS(const ABS& other);
     ABS& operator=(const ABS& rhs);
     ABS(ABS&& other) noexcept;
@@ -20,20 +28,44 @@ public:
     ~ABS() noexcept override;
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override;
+    [[nodiscard]] size_t getSize() const noexcept override{
+        return curr_size_;
+    }
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept;
+    [[nodiscard]] size_t getMaxCapacity() const noexcept{
+        return capacity_;
+    }
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept;
+    [[nodiscard]] T* getData() const noexcept{
+        return array_;
+    }
 
     // Push item onto the stack
-    void push(const T& data) override;
+    void push(const T& data) override{
+        if (curr_size_==capacity_){
+            capacity_*=2;
+            T* newArr = new T[capacity_];
+            for (int i = 0; i < curr_size_; i++){
+                newArr[i] = array_[i];
+            }
 
-    T peek() const override;
+            delete[] array_;
+            array_ = newArr;
+        }
+        array_[curr_size_] = data;
+        curr_size_++;
+    }
 
-    T pop() override;
+    T peek() const override{
+        return array_[curr_size_];
+    }
+
+    T pop() override{
+        curr_size_--;
+        return array_[curr_size_+1];
+    }
 
 private:
     size_t capacity_;
