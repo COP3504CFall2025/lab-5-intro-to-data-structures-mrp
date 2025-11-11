3b05fc72b86436837c32781896f254ccf5d99845
Michael Priore <162520748+mrp5623@users.noreply.github.com>
2025-11-11 19:16:54 +0000

    i should've pushed this in pieces this better work

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -18,27 +18,186 @@ private:
 
 public:
     // Big 5
-    ABDQ();
-    explicit ABDQ(std::size_t capacity);
-    ABDQ(const ABDQ& other);
-    ABDQ(ABDQ&& other) noexcept;
-    ABDQ& operator=(const ABDQ& other);
-    ABDQ& operator=(ABDQ&& other) noexcept;
-    ~ABDQ() override;
+    ABDQ(){
+        capacity_ = 4;
+        size_ = 0;
+        front_ = 0;
+        back_ = 0;
+        data_ = new T[capacity_];
+    }
+    explicit ABDQ(std::size_t capacity){
+        capacity_ = capacity;
+        size_ = 0;
+        front_ = 0;
+        back_ = 0;
+        data_ = new T[capacity_];
+    }
+    ABDQ(const ABDQ& other){
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        data_ = new T[capacity_];
+
+        for (size_t i = 0; i < size_; i++){
+            data_[i] = other.data_[i];
+        }
+    }
+    ABDQ(ABDQ&& other) noexcept{
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        data_ = other.data_;
+
+        other.capacity_ = 0;
+        other.size_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+        other.data_ = nullptr;
+    }
+    ABDQ& operator=(const ABDQ& other){
+        if (this == &other) return *this;
+
+        T* newArr = new T[other.capacity_];
+        delete[] data_;
+        
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        data_ = newArr;
+
+        for (size_t i = 0; i < size_; i++){
+            data_[i] = other.data_[i];
+        }
+
+        return *this;
+
+    }
+    ABDQ& operator=(ABDQ&& other) noexcept{
+        if (this == &other) return *this;
+        delete[] data_;
+        
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+        data_ = other.data_;
+
+        other.capacity_ = 0;
+        other.size_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+        other.data_ = nullptr;
+
+        return *this;
+    }
+    ~ABDQ() override{
+        capacity_ = 0;
+        size_ = 0;
+        front_ = 0;
+        back_ = 0;
+        delete[] data_;
+
+        data_ = nullptr;
+        
+    }
 
     // Insertion
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+        if (capacity_ == size_) {
+            capacity_*= SCALE_FACTOR;
+            T* newArr = new T[capacity_];
+            for (size_t i = 0; i < size_; i++){
+                newArr[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = newArr;
+        }
+
+        for (size_t i = size_; i>0; i--){
+            data_[i] = data_[i-1];
+        }
+
+        data_[0] = item;
+        size_++;
+    }
+    void pushBack(const T& item) override{
+        if (capacity_ == size_) {
+            capacity_*= SCALE_FACTOR;
+            T* newArr = new T[capacity_];
+            for (size_t i = 0; i < size_; i++){
+                newArr[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = newArr;
+        }
+
+        data_[size_] = item;
+        size_++;
+    }
 
     // Deletion
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+        if(size_==0) throw std::runtime_error("Deque Empty");
+
+        T front = data_[0];
+
+        for (size_t i = 1; i<size_; i++){
+            data_[i-1] = data_[i];
+        }
+
+        size_--;
+
+        if (size_ < capacity_/SCALE_FACTOR){
+            capacity_/=SCALE_FACTOR;
+            T* newArr = new T[capacity_];
+            for (size_t i = 0; i < size_; i++){
+                newArr[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = newArr;
+        }
+
+        return front;
+
+    }
+    T popBack() override{
+        if(size_==0) throw std::runtime_error("Deque Empty");
+
+        T back = data_[size_-1];
+
+        size_--;
+
+        if (size_ < capacity_/SCALE_FACTOR){
+            capacity_/=SCALE_FACTOR;
+            T* newArr = new T[capacity_];
+            for (size_t i = 0; i < size_; i++){
+                newArr[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = newArr;
+        }
+
+        return back;
+    }
 
     // Access
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{
+        if(size_==0) throw std::runtime_error("Deque Empty");
+
+        return data_[0];
+    }
+    const T& back() const override{
+        if(size_==0) throw std::runtime_error("Deque Empty");
+
+        return data_[size_-1];
+    }
 
     // Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return size_;
+    }
 
 };
