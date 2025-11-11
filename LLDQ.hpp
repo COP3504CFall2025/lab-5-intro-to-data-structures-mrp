93de86ef334ba8185d26b7f914fb488b1c4b2e4a
Michael Priore <162520748+mrp5623@users.noreply.github.com>
2025-11-11 18:30:07 +0000

    lldq first

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,52 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ(){}
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+        list.addHead(item);
+    }
+    void pushBack(const T& item) override{
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+        if (getSize()==0) throw std::runtime_error("Deque Empty");
+
+        T head_data = list.getHead()->data;
+
+        list.removeHead();
+
+        return head_data;
+    }
+    T popBack() override{
+        if (getSize()==0) throw std::runtime_error("Deque Empty");
+
+        T tail_data = list.getTail()->data;
+
+        list.removeTail();
+
+        return tail_data;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{
+        if (getSize()==0) throw std::runtime_error("Deque Empty");
+
+        return list.getHead();
+    }
+    const T& back() const override{
+        if (getSize()==0) throw std::runtime_error("Deque Empty");
+
+        return list.getTail();
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
 
 
