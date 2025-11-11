1857bbabf3bbac5db65b26fcd1ae407d3d49e8a9
Michael Priore <162520748+mrp5623@users.noreply.github.com>
2025-11-11 18:20:59 +0000

    LLQ first

--- LLQ.hpp
+++ LLQ.hpp
@@ -14,15 +14,30 @@ public:
     LLQ();
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T& item) override{
+        list.addTail(item);
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override{
+        if (!list.getHead()) throw std::runtime_error("Stack empty");
+
+        T head_data = list.getHead()->data;
+        
+        list.removeHead();
+        
+        return head_data;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override{
+        if (getSize()==0) throw std::runtime_error("Stack empty");
+        return list.getHead()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 
 };
